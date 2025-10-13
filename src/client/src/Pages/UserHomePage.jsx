8642ec4b9ac7c326d081fad5d0d44f22d3079d8b
Neh333 <nhughe19@kent.edu>
2025-10-13 12:44:41 -0400

    test commit

--- src/client/src/Pages/UserHomePage.jsx
+++ src/client/src/Pages/UserHomePage.jsx
@@ -7,32 +7,32 @@ import '../styles/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from "axios";
 
-
+//Testing push changes 
 
 function UserHomePage(){
-  const [users, setUsers] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true); // replaces `data`
+  // const [users, setUsers] = useState([]);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
 
- useEffect(() => {
-  axios.get("/api/users")
-    .then(response => {
-      setUsers(response.data); // Axios auto-parses JSON
-      setLoading(false);
-    })
-    .catch(err => {
-      setError(err);
-      setLoading(false);
-    });
-}, []);
+//  useEffect(() => {
+//   axios.get("/api/users")
+//     .then(response => {
+//       setUsers(response.data); // Axios auto-parses JSON
+//       setLoading(false);
+//     })
+//     .catch(err => {
+//       setError(err);
+//       setLoading(false);
+//     });
+//   }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // }
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
 
 
   return(
@@ -40,7 +40,7 @@ function UserHomePage(){
       {/* <h1>{message}</h1> */}
       <ReleaseBanner />
       <SequentialAnimations />
-
+{/* 
        <Container className="mt-4">
         <h2>User List</h2>
         <ul>
@@ -50,7 +50,7 @@ function UserHomePage(){
             </li>
           ))}
         </ul>
-      </Container>
+      </Container> */}
 
     </Container>);
 }
