3891185c5396f99265e8e890ec14db3ac637ba72
Neh333 <nhughe19@kent.edu>
2025-11-19 11:50:36 -0500

    1440pxw UHP responsivness progress

--- src/client/src/Pages/UserHomePage.jsx
+++ src/client/src/Pages/UserHomePage.jsx
@@ -1,6 +1,6 @@
 'use client';
-import React, {useState, useEffect} from "react"
-import {Container, Col, Row} from 'react-bootstrap'
+// import React, {useState, useEffect} from "react"
+import {Container} from 'react-bootstrap'
 import SequentialAnimations from "../components/SequentialAnimations.jsx"
 import ReleaseBanner from "../components/ReleaseBanner.jsx"
 import '../styles/App.css'
