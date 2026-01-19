0541b8e7ba8a808e33f0535a741e76ae3bde9706
gerbrian93 <gerbrian93@gmail.com>
2026-01-19 00:43:47 -0500

    Made product page menu collapsable with a button, since the splitscreen componenet I added controls how much of the page it takes, setting it to 0 removes it and makes the right side take up the whole screen.

--- src/client/src/Pages/ProductPage.jsx
+++ src/client/src/Pages/ProductPage.jsx
@@ -2,13 +2,22 @@ import React, { useState } from "react";
 import SplitScreen from "../components/SplitScreen";
 import CategorySelector from "../components/CategorySelector";
 import ProductDisplay from "../components/ProductDisplay";
+import { Button } from "react-bootstrap";
 
 function ProductPage() {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [ leftWeight, setLeftWeight] = useState(1);
+
+  const collapseSidebar = () => {
+  setLeftWeight(prev => (prev === 1 ? 0 : 1));
+};
+
 
   return (
     <div style={{ margin: 0, padding: 0 }}>
-      <SplitScreen leftWeight={1} rightWeight={4}>
+      <Button onClick={() => {collapseSidebar()}}>{leftWeight === 1 ? "Collapse Menu" : "Expand Menu"}</Button>
+      <SplitScreen leftWeight={leftWeight} rightWeight={4}>
+        
         <CategorySelector 
           onCategorySelect={setSelectedCategory}
           selectedCategory={selectedCategory}
